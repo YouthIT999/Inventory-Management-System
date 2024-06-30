e7162695807694bb8d12fac47f2a8b0e62c8f9f9
ByteMasterSamir <samiradhikary972@gmail.com>
2024-06-30 23:38:56 +0545

    router

--- server/app.js
+++ server/app.js
@@ -11,6 +11,3 @@ app.use(express.urlencoded({ extended: true }));
 app.listen(port, () => {
   console.log(`Server ran successfully on ${port}`);
 });
-
-console.log("Hello");
-console.log("Hello");
